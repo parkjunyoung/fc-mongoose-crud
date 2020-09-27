49454fdfabb42fcada7c80e7c179b9328bd53cb6
pjoon86 <pjoon86@naver.com>
2020-09-28 00:33:59 +0900

    CRUD 작성

--- controllers/admin/admin.ctrl.js
+++ controllers/admin/admin.ctrl.js
@@ -1,6 +1,8 @@
-exports.get_products =  ( _ , res) => {
-    
-    res.render( 'admin/products.html');
+const ProductsModel = require('../../models/Products');
+
+exports.get_products = async ( _ , res) => {
+    const products = await ProductsModel.find()
+    res.render( 'admin/products.html', { products });
 
 }
 
@@ -10,32 +12,37 @@ exports.get_products_write = ( _ , res) => {
 
 }
 
-exports.post_products_write =  ( req , res ) => {
+exports.post_products_write = async ( req , res ) => {
 
+    await ProductsModel.create(req.body);
     res.redirect('/admin/products');
 
 }
 
-exports.get_products_detail =  ( req , res ) => {
+exports.get_products_detail = async ( req , res ) => {
 
-    res.render('admin/detail.html');  
-
-};
-
-exports.get_products_edit =  ( req , res ) => {
-
-    res.render('admin/write.html');  
+    const product = await ProductsModel.findById(req.params.id);
+    res.render('admin/detail.html', { product });  
 
 };
 
-exports.post_products_edit =  ( req , res ) => {
+exports.get_products_edit = async ( req , res ) => {
 
-    
+    const product = await ProductsModel.findById(req.params.id);
+    res.render('admin/write.html', { product });  
+
+};
+
+exports.post_products_edit = async ( req , res ) => {
+
+    await ProductsModel.updateOne({ _id : req.params.id } , req.body );
+    res.redirect('/admin/products/detail/' + req.params.id );
 
 }
 
-exports.get_products_delete =  ( req , res ) => {
+exports.get_products_delete = async ( req , res ) => {
 
+    await ProductsModel.deleteOne({ _id : req.params.id });
     res.redirect('/admin/products');
     
 };
